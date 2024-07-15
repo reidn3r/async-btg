4a270224ce9ec37cca3800cc6b580c13e9100ffe
reidn3r <reidnersteam@hotmail.com>
2024-07-15 09:06:02 -0300

    feat: orders cache data

--- orders/src/http/app.ts
+++ orders/src/http/app.ts
@@ -4,6 +4,8 @@ import { RabbitMQ } from '../amqp/rabbitmq';
 import { redis } from '../../db/redis';
 dotenv.config();
 
+import { RedisService } from './services/redis-service';
+
 //Fastify Instance
 const app = fastify();
 
@@ -13,12 +15,15 @@ const queue:string = process.env.ORDERS_QUEUE as string;
 
 //Services instances
 const rabbitmq = new RabbitMQ(amqp_uri);
+const redisService = new RedisService();
 
 const PORT = 8000;
 app.listen({ port: PORT }, async() => {
     await Promise.all([rabbitmq.start(), redis.connect()])
-    await rabbitmq.consume(queue, (message) => {
-        console.log(message);
+    await rabbitmq.consume(queue, async(message) => {
+        const payload = JSON.parse(message.content.toString());
+        await redisService.execute(payload);
+        
     });
     console.log(`orders-service: http://localhost:${PORT}`);
 })
--- orders/src/http/services/redis-service.ts
+++ orders/src/http/services/redis-service.ts
@@ -1,25 +1,62 @@
 import { redis } from '../../../db/redis';
+import { MessagePayload } from '../../../../customers/src/http/adapter/types/message-type';
 
 export class RedisService {
-    /* 
-        Redis:
-            1. Armazenar produtos mais vendidos por quantidade (updateBestSellerProducts)
-            2. Armazenar produtos mais vendidos por valor
-            3. Armazenar clientes que mais gastaram
-            4. Armazenar top 5 pedidos mais caros
-            5. Armazenar 5 pedidos mais recentes *
-    */
+    private mro_list:string = process.env.MOST_RECENT_ORDERS_LIST as string;
+    private bspv_set:string = process.env.BEST_SELLER_PRODUCT_VALUE as string;
+    private bspq_set:string = process.env.BEST_SELLER_PRODUCT_QUANTITY as string;
+    private customer_value_set:string = process.env.CUSTOMER_VALUE_SET as string;
+    private orders_values_set:string = process.env.ORDERS_VALUE_SET as string;
 
-    async updateMostRecent(payload:string){
-        const message = JSON.parse(payload);
-        const llen:number = await redis.LLEN("list::most_recent_orders");
-        
-        if(llen > 5) await redis.RPOP("list::most_recent_orders");
-        return await redis.LPUSH("list::most_recent_orders", message.orderId);
+
+    async execute(payload:MessagePayload):Promise<void>{
+        try{
+            await Promise.all([
+                this.updateMostRecent(payload),
+                this.updateBestSellerProductsByValue(payload),
+                this.updateBestSellerProductsByQuantity(payload),
+                this.updateBestCustomers(payload),
+                this.updateBestCustomers(payload),
+                this.updateMostExpensiveOrders(payload)
+            ])
+        }
+        catch(err){
+            throw new Error("Error while updating cache data");
+        }
     }
 
-    async updateBestSellerProducts(payload:string){
-        const message = JSON.parse(payload);
-        const setSize:number = await redis.zCard("set::best_seller_products_value");
+    async updateMostRecent(payload:MessagePayload):Promise<void>{
+        const llen:number = await redis.LLEN(this.mro_list);
+        if(llen > 20) await redis.RPOP(this.mro_list);
+        await redis.LPUSH(this.mro_list, payload.orderId);
+    }
+
+    async updateBestSellerProductsByQuantity(payload:MessagePayload):Promise<void>{
+        const items = payload.items.map((i) => { return { id: i.id, quantity: i.quantity }});
+        for(const item of items){
+            await redis.zIncrBy(this.bspq_set, item.quantity, item.id);
+        };
+    }
+
+    async updateBestSellerProductsByValue(payload:MessagePayload):Promise<void>{
+        const items = payload.items.map((i) => { return { id: i.id, quantity: i.quantity, value: i.value }});
+        for(const item of items){
+            await redis.zIncrBy(this.bspv_set, item.quantity * item.value, item.id);
+        };
+    }
+    
+    async updateBestCustomers(payload:MessagePayload):Promise<void>{
+        const value = this.calculateOrderValue(payload);
+        await redis.zIncrBy(this.customer_value_set, value, payload.customerId);
+    }
+    
+    async updateMostExpensiveOrders(payload:MessagePayload):Promise<void>{
+        const value = this.calculateOrderValue(payload);
+        await redis.zAdd(this.orders_values_set, { score: value, value: payload.customerId});
+    }
+    
+    
+    private calculateOrderValue(payload:MessagePayload):number{
+        return payload.items.reduce((acc, item) => acc + item.quantity * item.value, 0);
     }
 }
